e98bf893fb1b2856ddd9e3730d656fab2176e170
Sebastián Audetat <sebastian.audetat.f@gmail.com>
2021-04-17 19:24:21 -0600

    Feature: Init HomePage Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import HomePage from "../pages/HomePage";
 
 function App() {
-  return <div>My first App using React</div>;
+  return <HomePage />;
 }
 
 export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage(props) {
+  return (
+    <>
+      <h1>HOME PAGE</h1>
+    </>
+  );
+}
+
+export default HomePage;
