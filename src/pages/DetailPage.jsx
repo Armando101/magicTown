4fb059f10a4bd55f5ec6d8dfb51a04286e47f68f
Armando Rivera <rivera.armando997@gmail.com>
2021-04-22 14:26:11 -0500

    feature/Configure webpack for react-router and detail page structure

---
+++ src/pages/DetailPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DetailPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>DetailPage Works!!!</p>
+    </div>
+  );
+};
+
+export default DetailPage;
