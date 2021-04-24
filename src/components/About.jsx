3d9528569d93933cb0984e88c2e5e6dd6027fad7
Armando Rivera <rivera.armando997@gmail.com>
2021-04-24 14:42:01 -0500

    cambios

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "@styles/components/About.scss";
 
-const About = ({ name, stars, description }) => {
+const About = ({ name, stars, description, maps }) => {
   console.log(name, stars, description);
   const starsArr = [...Array(stars).keys()];
   return (
@@ -25,7 +25,7 @@ const About = ({ name, stars, description }) => {
         className="about__map"
         dangerouslySetInnerHTML={{
           __html: `<iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29850.42358307275!2d-99.95686182162947!3d20.738511663990145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d37edfe530e957%3A0xaa1f4884f962782c!2sBernal%2C%20Qro.!5e0!3m2!1ses-419!2smx!4v1619123627952!5m2!1ses-419!2smx"
+              src=${maps}
               width="100%"
               height="450"
               style="border:0;"
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -13,6 +13,8 @@ const details = [
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas vero asperiores labore laborum culpa velit accusantium eius ipsam quam expedita sed ea, illo quibusdam nam dolorum ipsa id. Veniam, voluptates?Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, tempora officia eveniet minima, ipsum pariatur enim perspiciatis velit iusto eos, architecto similique a reprehenderit ad debitis dolores. Sapiente nesciunt consequatur ad molestias exercitationem accusantium id quaerat iusto sunt quibusdam, veritatis tempora libero, commodi minus alias rerum. Possimus maxime dolores atque.",
     cover:
       "https://www.eluniversal.com.mx/sites/default/files/2019/07/26/taxco_de_alarcon_pueblo_magico_0.jpg",
+    maps:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29850.42358307275!2d-99.95686182162947!3d20.738511663990145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d37edfe530e957%3A0xaa1f4884f962782c!2sBernal%2C%20Qro.!5e0!3m2!1ses-419!2smx!4v1619123627952!5m2!1ses-419!2smx",
   },
   {
     id: 1,
@@ -22,6 +24,8 @@ const details = [
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas vero asperiores labore laborum culpa velit accusantium eius ipsam quam expedita sed ea, illo quibusdam nam dolorum ipsa id. Veniam, voluptates?Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, tempora officia eveniet minima, ipsum pariatur enim perspiciatis velit iusto eos, architecto similique a reprehenderit ad debitis dolores. Sapiente nesciunt consequatur ad molestias exercitationem accusantium id quaerat iusto sunt quibusdam, veritatis tempora libero, commodi minus alias rerum. Possimus maxime dolores atque.",
     cover:
       "https://www.eluniversal.com.mx/sites/default/files/2019/07/26/taxco_de_alarcon_pueblo_magico_0.jpg",
+    maps:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29850.42358307275!2d-99.95686182162947!3d20.738511663990145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d37edfe530e957%3A0xaa1f4884f962782c!2sBernal%2C%20Qro.!5e0!3m2!1ses-419!2smx!4v1619123627952!5m2!1ses-419!2smx",
   },
   {
     id: 2,
@@ -31,6 +35,8 @@ const details = [
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas vero asperiores labore laborum culpa velit accusantium eius ipsam quam expedita sed ea, illo quibusdam nam dolorum ipsa id. Veniam, voluptates?Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, tempora officia eveniet minima, ipsum pariatur enim perspiciatis velit iusto eos, architecto similique a reprehenderit ad debitis dolores. Sapiente nesciunt consequatur ad molestias exercitationem accusantium id quaerat iusto sunt quibusdam, veritatis tempora libero, commodi minus alias rerum. Possimus maxime dolores atque.",
     cover:
       "https://www.eluniversal.com.mx/sites/default/files/2019/07/26/taxco_de_alarcon_pueblo_magico_0.jpg",
+    maps:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29850.42358307275!2d-99.95686182162947!3d20.738511663990145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d37edfe530e957%3A0xaa1f4884f962782c!2sBernal%2C%20Qro.!5e0!3m2!1ses-419!2smx!4v1619123627952!5m2!1ses-419!2smx",
   },
   {
     id: 3,
@@ -40,6 +46,8 @@ const details = [
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas vero asperiores labore laborum culpa velit accusantium eius ipsam quam expedita sed ea, illo quibusdam nam dolorum ipsa id. Veniam, voluptates?Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, tempora officia eveniet minima, ipsum pariatur enim perspiciatis velit iusto eos, architecto similique a reprehenderit ad debitis dolores. Sapiente nesciunt consequatur ad molestias exercitationem accusantium id quaerat iusto sunt quibusdam, veritatis tempora libero, commodi minus alias rerum. Possimus maxime dolores atque.",
     cover:
       "https://www.eluniversal.com.mx/sites/default/files/2019/07/26/taxco_de_alarcon_pueblo_magico_0.jpg",
+    maps:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29850.42358307275!2d-99.95686182162947!3d20.738511663990145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d37edfe530e957%3A0xaa1f4884f962782c!2sBernal%2C%20Qro.!5e0!3m2!1ses-419!2smx!4v1619123627952!5m2!1ses-419!2smx",
   },
   {
     id: 4,
@@ -49,6 +57,8 @@ const details = [
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas vero asperiores labore laborum culpa velit accusantium eius ipsam quam expedita sed ea, illo quibusdam nam dolorum ipsa id. Veniam, voluptates?Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, tempora officia eveniet minima, ipsum pariatur enim perspiciatis velit iusto eos, architecto similique a reprehenderit ad debitis dolores. Sapiente nesciunt consequatur ad molestias exercitationem accusantium id quaerat iusto sunt quibusdam, veritatis tempora libero, commodi minus alias rerum. Possimus maxime dolores atque.",
     cover:
       "https://www.eluniversal.com.mx/sites/default/files/2019/07/26/taxco_de_alarcon_pueblo_magico_0.jpg",
+    maps:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29850.42358307275!2d-99.95686182162947!3d20.738511663990145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d37edfe530e957%3A0xaa1f4884f962782c!2sBernal%2C%20Qro.!5e0!3m2!1ses-419!2smx!4v1619123627952!5m2!1ses-419!2smx",
   },
 ];
 
