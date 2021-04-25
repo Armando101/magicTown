5630459a7a95939b5607e47ad7695cbb565b09d6
Armando Rivera <rivera.armando997@gmail.com>
2021-04-25 11:19:42 -0500

    Fix bug

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Avatar from "./Avatar";
-import Rating from '@material-ui/lab/Rating';
+import Rating from "@material-ui/lab/Rating";
 // import Typography from '@material-ui/core/Typography';
 // import Box from '@material-ui/core/Box';
-
 
 import "../styles/components/ReviewCard.scss";
 import { userInfo } from "../dummyData";
@@ -16,14 +15,13 @@ function ReviewCard({ reviewInfo }) {
       <div className="reviewcard__body">
         <header className="reviewcard__header">
           <div className="reviewcard_stars">
-            <h4>{reviewInfo.townName}</h4> <Rating name="read-only" value={reviewInfo.rate} readOnly />
-          </div> {reviewInfo.rate}
+            <h4>{reviewInfo.townName}</h4>{" "}
+            <Rating name="read-only" value={reviewInfo.rate} readOnly />
+          </div>
         </header>
         <q className="reviewcard__copy"> {reviewInfo.description} </q>
         <div className="author">
-          <cite>
-            {userInfo.username}
-          </cite>
+          <cite>{userInfo.username}</cite>
         </div>
       </div>
     </div>
